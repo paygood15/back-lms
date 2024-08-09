bcf45850a082ca30b09e86b1836b4d07b5cc4c01
paygood15 <167597519+paygood15@users.noreply.github.com>
2024-08-10 02:08:10 +0300

    Update authRoute.js

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -5,12 +5,14 @@ const {
   forgotPassword,
   verifyPasswordResetCode,
   resetPassword,
+  logout,
 } = require("../controllers/authController");
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
+router.post("/logout", logout);
 router.post("/forgotPasswords", forgotPassword);
 router.post("/verifyResetCode", verifyPasswordResetCode);
 router.put("/resetPassword", resetPassword);
